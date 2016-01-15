54065d87a17ebe5a64dca51f39c1d6291791883e
Nolan Lawson <nolan.lawson@gmail.com>
2016-01-14 23:57:02 -0500

    more fixes

--- README.md
+++ README.md
@@ -3,12 +3,23 @@ cjs-to-es6
 
 CLI to convert JavaScript files from CommonJS to ES6 modules (aka ES2015 modules, aka JavaScript modules, aka `import`/`export` instead of `require`/`module.exports`), using [5to6-codemod](https://github.com/5to6/5to6-codemod).
 
-Usage
+Installation
 ---
 
     npm install -g cjs-to-es6
 
-Convert a file:
+Usage
+---
+
+```
+Usage: index.js [ files/directories ... ]
+
+Options:
+  -h, --help  show help message                                        [boolean]
+  --verbose   show verbose output                     [boolean] [default: false]
+```
+
+Convert a single file:
 
     cjs-to-es6 index.js
 
@@ -20,4 +31,45 @@ Convert all files in a directory:
 
     cjs-to-es6 lib/
     
-It modifies the files in-place. That's it.
+The files are modified in-place.
+
+Migrating from CommonJS to ES6 modules
+--------
+
+Not all CommonJS patterns have 1-to-1 equivalents in ES6 modules.
+So you may have to correct some errors manually.
+
+Use `--verbose` to get detailed output, or follow these general tips:
+
+### `export`s must be at the top level
+
+This is invalid:
+
+```js
+if (clownShoes) {
+  export default new Clown();
+} else {
+  export default new RespectableGentleman();
+}
+```
+
+Instead do:
+
+```js
+var exported = clownShoes ? new Clown() : new RespectableGentleman();
+export default exported;
+```
+
+### `import`s also must be at the top level
+
+This is invalid:
+
+```js
+try {
+  import Nothing from 'bad-module';
+} catch (err) {
+  console.log('oh noes!');
+}
+```
+
+There is no equivalent for this `try`/`catch` pattern in ES6 modules.
--- index.js
+++ index.js
@@ -4,25 +4,31 @@ var lie = require('lie');
 if (typeof Promise === 'undefined') {
   global.Promise = lie;
 }
-var denodeify = requre('denodeify');
-var findit = denodeify(require('findit'));
+var denodeify = require('denodeify');
+var findit = require('findit');
 var fs = require('fs');
 var statAsync = denodeify(fs.stat);
 var existsAsync = denodeify(fs.exists);
-var exec = require('child-process-promise').exec;
+var spawn = require('child-process-promise').spawn;
 var flatten = require('lodash.flatten');
+var uniq = require('uniq');
 var path = require('path');
+var colors = require('colors/safe');
 var yargs = require('yargs')
   .usage('Usage: $0 [ files/directories ... ]')
   .boolean('h')
   .alias('h', 'help')
-  .describe('h', 'this help message')
+  .describe('h', 'show help message')
+  .boolean('verbose')
+  .describe('verbose', 'show verbose output')
+  .default('verbose', false)
   .example('$0 index.js', 'convert a single file')
   .example('$0 lib/', 'convert all files in a directory')
   .example('$0 foo.js bar.js lib/', 'convert many files/directories')
   ;
 
 var files = yargs.argv._;
+var verbose = yargs.argv.verbose;
 
 if (yargs.argv.h || !files.length) {
   console.log('\ncjs-to-es6 v' + require('./package.json').version + ': ' +
@@ -31,7 +37,54 @@ if (yargs.argv.h || !files.length) {
   process.exit(0);
 }
 
+function findJsFiles(dir) {
+  return new Promise(function (resolve, reject) {
+    var files = [];
+    findit(dir).on('file', function (file) {
+      // only return files ending in .js
+      if (/\.js$/.test(file)) {
+        files.push(file);
+      }
+    }).on('end', function () {
+      resolve(files);
+    }).on('error', reject);
+  });
+}
+
+function runCodeshift(transformName, files) {
+  var cmd = require.resolve("jscodeshift/bin/jscodeshift.sh");
+  var transform = require.resolve('5to6-codemod/transforms/' + transformName);
+  var child = spawn(cmd, ["-t", transform].concat(files));
+  child.progress(function (childProcess) {
+    if (verbose) {
+      childProcess.stdout.pipe(process.stdout);
+    } else {
+      childProcess.stdout.on('data', function (data) {
+        if (/^Results: /.test(String(data))) {
+          console.log(String(data).replace(/\n$/, ''));
+        }
+      });
+    }
+    childProcess.stderr.pipe(process.stderr);
+  });
+  return child;
+}
+
+function derequireify(files) {
+  console.log('\nTransforming ' + colors.yellow('require()') + ' to ' +
+    colors.cyan('import') + ' ...');
+  return runCodeshift('cjs.js', files);
+}
+
+function deexportify(files) {
+  console.log('\nTransforming ' + colors.yellow('module.exports') + '/' +
+    colors.red('exports') + ' to ' +
+    colors.cyan('export') + ' ...');
+  return runCodeshift('exports.js', files);
+}
+
 Promise.resolve().then(function () {
+  console.log(colors.rainbow('\nAhoy!') + ' ES6ifyin\' your CommonJS for ya...');
   return Promise.all(files.map(function (file) {
     file = path.resolve(file);
     return existsAsync(file).catch(function (exists) {
@@ -42,26 +95,29 @@ Promise.resolve().then(function () {
       return statAsync(file);
     }).then(function (stat) {
       if (stat.isDirectory()) {
-        return findit(file);
+        return findJsFiles(file);
       }
       return [file];
     });
-  })).then(flatten);
+  })).then(flatten).then(uniq);
 }).then(function (files) {
-  var cmd = require.resolve('es6-codemod/transforms/cjs.js');
-  return exec(cmd, files)
+  console.log('\nFound ' + colors.cyan(files.length.toString()) + ' files.');
+  return derequireify(files).then(function () {
+    return deexportify(files);
+  })
 }).catch(function (err) {
-  console.log(err);
+  if (err.errno == 'E2BIG') {
+    throw new Error('Sorry, too many files at once');
+  }
+  throw err;
+}).then(function () {
+  console.log(colors.rainbow('\nES6ification complete!'));
+  if (!verbose) {
+    console.log('Re-run with ' + colors.cyan('--verbose') +
+      ' to see full output.');
+  }
+  console.log();
+}).catch(function (err) {
+  console.log(err.stack);
   process.exit(1);
-});
-
-files.ForEach(function (file) {
-  file = path.resolve(file);
-  if (!fs.existsSync(file)) {
-    throw new Error('file not found: ' + file);
-  }
-  if (fs.statSync(file).isDirectory()) {
-    return findit(file);
-  }
-  return file;
 });
