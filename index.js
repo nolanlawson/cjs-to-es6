df08eb0d9cd03f7be0fa832a541a78bb965ca6f6
Michael Adams <unquietwiki@gmail.com>
2022-03-03 07:21:36 -0800

    fix: complete overhaul
    * Complete overhaul
    
    * Fix missing sort function
    
    * Add resolutions for submodules
    
    * Nuke submodules
    
    * nuke submodules

--- index.js
+++ index.js
@@ -1,127 +1,138 @@
 #!/usr/bin/env node
 
-if (typeof Promise === 'undefined') {
-  var lie = require('lie');
-  global.Promise = lie;
-}
-var denodeify = require('denodeify');
-var findit = require('findit');
-var fs = require('fs');
-var statAsync = denodeify(fs.stat);
-var existsAsync = denodeify(fs.exists);
-var spawn = require('child-process-promise').spawn;
-var flatten = require('lodash.flatten');
-var uniq = require('uniq');
-var path = require('path');
-var colors = require('colors/safe');
-var yargs = require('yargs')
-  .usage('Usage: $0 [ files/directories ... ]')
-  .boolean('h')
-  .alias('h', 'help')
-  .describe('h', 'show help message')
-  .boolean('verbose')
-  .describe('verbose', 'show verbose output')
-  .default('verbose', false)
-  .alias('p', 'parser')
-  .describe('p', 'choose the parser that should be used with jscodeshift')
-  .choices('p', ['babel', 'babylon', 'flow'])
-  .default('p', 'babel')
-  .example('$0 index.js', 'convert a single file')
-  .example('$0 lib/', 'convert all files in a directory')
-  .example('$0 foo.js bar.js lib/', 'convert many files/directories')
-  ;
-
-var files = yargs.argv._;
-var verbose = yargs.argv.verbose;
-
-if (yargs.argv.h || !files.length) {
-  console.log('\ncjs-to-es6 v' + require('./package.json').version + ': ' +
-    require('./package.json').description + '\n');
-  yargs.showHelp();
-  process.exit(0);
-}
-
-function findJsFiles(dir) {
-  return new Promise(function (resolve, reject) {
-    var files = [];
-    findit(dir).on('file', function (file) {
-      // only return files ending in .js
-      if (/\.js$/.test(file)) {
-        files.push(file);
-      }
-    }).on('end', function () {
-      resolve(files);
-    }).on('error', reject);
-  });
-}
-
-function runCodeshift(transformName, files) {
-  var cmd = require.resolve("jscodeshift/bin/jscodeshift.sh");
-  var transform = require.resolve('5to6-codemod/transforms/' + transformName);
-  var child = spawn(cmd, ["--parser", yargs.argv.parser, "-t", transform].concat(files));
-  child.progress(function (childProcess) {
-    if (verbose) {
-      childProcess.stdout.pipe(process.stdout);
-    } else {
-      childProcess.stdout.on('data', function (data) {
-        if (/^Results: /.test(String(data))) {
-          console.log(String(data).replace(/\n$/, ''));
-        }
-      });
+const colors = require('colors/safe')
+const os = require('os')
+const path = require('path')
+const readdirp = require('readdirp')
+const spawn = require('child-process-promise').spawn
+const yargs = require('yargs')
+  .options({
+    h: {
+      type: 'boolean',
+      alias: ['h', 'help'],
+      describe: 'show help message',
+      default: false
+    },
+    v: {
+      type: 'boolean',
+      alias: ['v', 'verbose'],
+      describe: 'show verbose output',
+      default: false
+    },
+    p: {
+      alias: ['p', 'parser'],
+      type: 'string',
+      describe: 'choose the parser that should be used with jscodeshift',
+      choices: ['babel', 'babylon', 'flow', 'ts', 'tsx'],
+      default: 'babel'
     }
-    childProcess.stderr.pipe(process.stderr);
-  });
-  return child;
-}
-
-function derequireify(files) {
-  console.log('\nTransforming ' + colors.yellow('require()') + ' to ' +
-    colors.cyan('import') + ' ...');
-  return runCodeshift('cjs.js', files);
-}
-
-function deexportify(files) {
-  console.log('\nTransforming ' + colors.yellow('module.exports') + '/' +
-    colors.red('exports') + ' to ' +
-    colors.cyan('export') + ' ...');
-  return runCodeshift('exports.js', files);
-}
-
-Promise.resolve().then(function () {
-  console.log(colors.rainbow('\nAhoy!') + ' ES6ifyin\' your CommonJS for ya...');
-  return Promise.all(files.map(function (file) {
-    file = path.resolve(file);
-    return existsAsync(file).catch(function (exists) {
-      if (!exists) {
-        throw new Error('file not found: ' + file);
-      }
-    }).then(function () {
-      return statAsync(file);
-    }).then(function (stat) {
-      if (stat.isDirectory()) {
-        return findJsFiles(file);
-      }
-      return [file];
-    });
-  })).then(flatten).then(uniq);
-}).then(function (files) {
-  console.log('\nFound ' + colors.cyan(files.length.toString()) + ' files.');
-  return derequireify(files).then(function () {
-    return deexportify(files);
   })
-}).catch(function (err) {
-  if (err.errno == 'E2BIG') {
-    throw new Error('Sorry, too many files at once');
+  .example([
+    ['$0 index.js', 'convert a single file'],
+    ['$0 lib/', 'convert all files in a directory'],
+    ['$0 foo.js bar.js lib/', 'convert many files/directories']
+  ])
+
+const argv = yargs.argv
+const filesToProcess = argv._
+
+if (argv.h || !yargs) {
+  console.log(`\ncjs-to-es6 v${require('./package.json').version}: ${require('./package.json').description}\n`)
+  yargs.showHelp()
+  process.exit(0)
+}
+
+function runCodeshift (transformName, files) {
+  try {
+    const cmd = require.resolve('jscodeshift/bin/jscodeshift.sh')
+    const transform = require.resolve(transformName)
+    const child = spawn(cmd, ['-c', os.cpus.length, '--parser', argv.p, '-t', transform].concat(files))
+    child.progress((childProcess) => {
+      if (yargs.v) {
+        childProcess.stdout.pipe(process.stdout)
+      } else {
+        childProcess.stdout.on('data', (data) => {
+          if (/^Results: /.test(String(data))) {
+            console.log(String(data).replace(/\n$/, ''))
+          }
+        })
+      }
+      childProcess.stderr.pipe(process.stderr)
+    })
+    return child
+  } catch (error) {
+    console.error(error)
+    return Promise.reject(error)
   }
-  throw err;
-}).then(function () {
-  console.log(colors.rainbow('\nES6ification complete!'));
-  if (!verbose) {
-    console.log('Re-run with ' + colors.cyan('--verbose') +
-      ' to see full output.');
+}
+
+async function codeSwitching (files) {
+  // Require -> Import
+  console.log(`Transforming ${colors.yellow('require()')} to ${colors.cyan('import')} ...`)
+  await runCodeshift('5to6-codemod/transforms/cjs.js', files)
+
+  // Sort imports
+  console.log(colors.magenta('\nSorting imports...\n'))
+  await runCodeshift('js-import-sort/index.js', files)  
+
+  // module.exports -> Export
+  console.log(`Transforming ${colors.yellow('module.exports')}/${colors.red('exports')} to ${colors.cyan('export')} ...`)
+  await runCodeshift('5to6-codemod/transforms/exports.js', files)
+  
+  // Safe function to arrow conversion
+  console.log(`Transforming (safely) ${colors.yellow('function ()')} to ${colors.cyan('() =>')} ...`)
+  await runCodeshift('next-js-codemod/transforms/arrow-function.js', files)
+
+  // Use const & let
+  console.log(`Transforming ${colors.yellow('var')} to ${colors.red('const')} or ${colors.cyan('let')} ...`)
+  await runCodeshift('next-js-codemod/transforms/no-vars.js', files)
+
+  // Implement ES2015+ object shorthand
+  console.log(colors.magenta('\nImplementing object shorthand...\n'))
+  await runCodeshift('next-js-codemod/transforms/rm-object-assign.js', files)
+
+  // Fix quoted properties
+  console.log(colors.magenta('\nFixing quoted properties...\n'))
+  await runCodeshift('next-js-codemod/transforms/unquote-properties.js', files)
+
+  // Update computed properties
+  console.log(colors.magenta('\nUpdating computed properties...\n'))
+  await runCodeshift('next-js-codemod/transforms/updated-computed-props.js', files)
+
+  // Convert lodash & underscore functions to ES2015+ native functions
+  console.log(colors.magenta('\nConvert lodash & underscore to ES2015+ functions...\n'))
+  await runCodeshift('next-js-codemod/transforms/underscore-to-lodash-native.js', files)
+}
+
+Promise.resolve().then(async () => {
+  console.log(`${colors.rainbow('\nAhoy!')} ES6ifyin' your CommonJS for ya...`)
+  const filesToLoad = []
+  for (const item of filesToProcess) {
+    try {
+      for await (const entry of readdirp(item, { fileFilter: ['*.js', '*.cjs'] })) {
+        filesToLoad.push(entry.fullPath)
+      }
+    }
+    catch {
+      filesToLoad.push(path.resolve(item))
+    }
   }
-  console.log();
-}).catch(function (err) {
-  console.log(err.stack);
-  process.exit(1);
-});
+  return filesToLoad
+}).then(async (files) => {
+  console.log(`\nFound ${colors.cyan(files.length.toString())} files.`)
+  return codeSwitching(files)
+}).catch((err) => {
+  if (err.errno === 'E2BIG') {
+    throw new Error('Sorry, too many files at once')
+  }
+  throw err
+}).then(() => {
+  console.log(colors.rainbow('\nES6ification complete!'))
+  if (!argv.v) {
+    console.log(`Re-run with ${colors.cyan('--verbose')} to see full output.`)
+  }
+  console.log()
+}).catch((err) => {
+  console.error(err.stack)
+  process.exit(1)
+})
